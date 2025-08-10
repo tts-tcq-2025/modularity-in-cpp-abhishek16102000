35aaa7d0e7fc4d954d5ed92c34cc7f21402ea2d5
abhishek16102000 <84512560+abhishek16102000@users.noreply.github.com>
2025-08-10 18:10:47 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <assert.h>
+#include "ColorCode.h"
+#include "ManualPrinter.h"
+
+using namespace TelCoColorCoder;
+
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    // Print the new color code manual
+    PrintColorCodeManual();
+
+    return 0;
+}
